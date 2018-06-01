d0dc94e3c2dfb0dcb6c3b257f16e7eda74ac3ed7
Lucas Nunes <lucas.nunes@eng.ci.ufpb.br>
2018-06-01 04:16:26 -0300

    Conclusão Aplicação Cadastro de Tarefas

---
+++ FrontEnd/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Our History</h2>
+        <p>Lorem Ipsum Dolor Sit Amet...</p>
+        <h2>Contact Us</h2>
+        <p>Lorem Ipsum Solor Sit Amet...</p>
+        <h2>Ending Board</h2>
+        <p>NING EUM MERECE ESC REVER MAIS ESSA PORCARIA </p>
+    </div>
+)
